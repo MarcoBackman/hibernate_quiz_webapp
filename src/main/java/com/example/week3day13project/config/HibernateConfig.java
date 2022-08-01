729ba21823cca7c370a39da37797b75cf35f4529
MarcoBackman <roni2006@hanmail.net>
2022-08-01 16:15:58 -0400

    Image updated

--- src/main/java/com/example/week3day13project/config/HibernateConfig.java
+++ src/main/java/com/example/week3day13project/config/HibernateConfig.java
@@ -2,9 +2,7 @@ package com.example.week3day13project.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
